23c49ecce2ae55ab3b0bf8d71579f22cddeaf88c
EMozikov <mozikoff@rambler.ru>
2022-04-29 17:02:00 +0300

    - added reflection helper

---
+++ src/main/java/helpers/ReflectionHelper.java
@@ -0,0 +1,37 @@
+package helpers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+public class ReflectionHelper {
+    private static final Logger logger = LogManager.getLogger(ReflectionHelper.class.getName());
+
+    public static Object createInstance(String className) {
+        Object obj = null;
+        try {
+            obj = Class.forName(className).getDeclaredConstructor().newInstance();
+        } catch (ClassNotFoundException | NoSuchMethodException |
+                InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            logger.error(e.getMessage());
+        }
+        return obj;
+    }
+
+    public static void setFieldValue(Object object, String fieldName, String fieldValue) {
+        try {
+            Field field = object.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+
+            if (field.getType().equals(String.class)) {
+                field.set(object, fieldValue);
+            } else if (field.getType().equals(int.class)) {
+                field.set(object, Integer.decode(fieldValue));
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
